3c7e88fce1a7b13b16de832dc909a09534822c5d
mdafonte <mdafonte@sicfe.com.uy>
2017-07-26 11:27:46 -0300

    Attribute directives

--- Introduction to angular/my-app/src/app/tasks.component.ts
+++ Introduction to angular/my-app/src/app/tasks.component.ts
@@ -3,12 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'tasks',
   template: `
-  <h4>task</h4>
-  `
+  <h4 [class.red]="toggle">task</h4>
+  <h4 [ngClass]="{red: toggle, blue:!toggle}">task</h4>
+  `,
+  styles:[".red {color: red}", ".blue {color: blue}"]
 })
 
 export class TasksComponent implements OnInit {
   constructor() { }
-
+  toggle: boolean = true;
   ngOnInit() { }
 }
